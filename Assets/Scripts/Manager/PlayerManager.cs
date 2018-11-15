79cd6fb5a299b872b93f731b1df890cfed8c7abd
boomerangll <yaronmironboomerangll@gmail.com>
2018-11-15 13:23:47 +0200

    123

---
+++ Assets/Scripts/Manager/PlayerManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerManager : MonoBehaviour {
+    public GameObject player;                // The enemy prefab to be spawned.
+    public Transform spawnLocation;         // An array of the spawn points this enemy can spawn from.
+
+    void Start() {
+        Spawn();
+    }
+
+    private void Spawn() {
+        Instantiate(player, spawnLocation.position, spawnLocation.rotation);
+    }
+}
